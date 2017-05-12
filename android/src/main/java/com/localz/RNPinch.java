0ec0fa8b5f2eec834db6b81bda192c18373f00cf
Admin <admin@gavin-laptop.redant.local>
2017-05-12 14:46:56 +0100

    Remove System.out.println

--- android/src/main/java/com/localz/RNPinch.java
+++ android/src/main/java/com/localz/RNPinch.java
@@ -108,8 +108,6 @@ public class RNPinch extends ReactContextBaseJavaModule {
                 JSONObject jsonHeaders = new JSONObject(httpResponse.headers.toString());
                 final String parsedBodyString = new String(httpResponse.bodyString, 'ISO-8859-1');
 
-                System.out.println(parsedBodyString);
-
                 response.putInt("status", httpResponse.statusCode);
                 response.putString("statusText", httpResponse.statusText);
                 response.putString("bodyString", parsedBodyString);
