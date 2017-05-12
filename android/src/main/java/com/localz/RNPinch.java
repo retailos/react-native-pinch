c7a9bd93491da87e37c080865610a928fe19d9f5
Admin <admin@gavin-laptop.redant.local>
2017-05-12 14:59:08 +0100

    Doublequotes and bump npm version

--- android/src/main/java/com/localz/RNPinch.java
+++ android/src/main/java/com/localz/RNPinch.java
@@ -107,7 +107,7 @@ public class RNPinch extends ReactContextBaseJavaModule {
 
                 HttpResponse httpResponse = httpUtil.sendHttpRequest(request);
                 JSONObject jsonHeaders = new JSONObject(httpResponse.headers.toString());
-                final String parsedBodyString = URLEncoder.encode(httpResponse.bodyString, 'ISO-8859-1');
+                final String parsedBodyString = URLEncoder.encode(httpResponse.bodyString, "ISO-8859-1");
 
                 response.putInt("status", httpResponse.statusCode);
                 response.putString("statusText", httpResponse.statusText);
